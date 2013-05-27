bedae7f009b4e52910568ecd26fc2d1c8c0acda1
John Shaw <j2897@mailinator.com>
2013-05-27 23:05:50 +0100

    Minor bug fixes.
    Added: End program if First Target isn't found.
    Renamed: pickle.dat to TOR_Updater.dat.
    Added: Check to see if TOR is running before updating.

--- Update_TOR.py
+++ Update_TOR.py
@@ -10,7 +10,7 @@ def get_page(page):
 def find_site_ver(page):
 	FT = page.find(target)		# First Target
 	if FT == -1:
-		return None				# Remember to implement something after here.
+		return None
 	TB = page.find('tor-browser-', FT)	# Tor Browser
 	AB = page.find('>', TB)				# Angle Bracket
 	return page[TB:AB-1]				# tor-browser-2.3.25-6_en-US.exe
@@ -19,12 +19,19 @@ target = 'Tor Browser Bundle for Windows'
 url = 'https://www.torproject.org/projects/torbrowser.html.en'
 file_url = 'https://www.torproject.org/dist/torbrowser/'
 page = get_page(url)
-
 home = os.getenv('USERPROFILE')
 site_version = find_site_ver(page)
+
+def stop():
+	import sys
+	sys.exit()
+
+if site_version == None:
+	stop()
+
 tmp = os.getenv('TEMP') + '\\'
 DL = tmp + site_version
-PC = home + '\\pickle.dat'
+PC = home + '\\TOR_Updater.dat'
 
 def clean(text):
 	import re
@@ -65,13 +72,9 @@ def sub_proc(sz_exe, args):
 	stdout, stderr = p.communicate()
 	return p.returncode # is 0 if success
 
-def stop():
-	import sys
-	sys.exit()
-
 # Check if Pickle Cache file exists...
 if not os.path.isfile(PC):
-	# No:	Download and install TOR, create a Pickle Cache file in the Python Script folder, dump site_version and quit.
+	# No:	Download and install TOR, create a Pickle Cache file in the home folder, dump site_version and quit.
 	DL_file(file_url, site_version, DL, tmp)
 	sz_exe, args = get_sz(tmp, DL)
 	sub_proc(sz_exe, args)
@@ -85,6 +88,22 @@ local_version = load()
 if clean(local_version).find(clean(site_version)) != -1:
 	# Yes:	Quit.
 	stop()
+
+# Check if TOR is running...
+def find_proc(exe):
+	import subprocess
+	cmd = 'WMIC PROCESS get Caption'
+	proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+	for line in proc.stdout:
+		if line.find(exe) != -1:
+			return True
+
+while find_proc('vidalia.exe'):
+	user_input = win32api.MessageBox(0, 'There is a new version of the TOR Browser Bundle available. Please close TOR and press OK to continue.', 'TOR Updater', 1)
+	if user_input == 1:
+		pass
+	elif user_input == 2:
+		stop()
 
 # Download and install TOR, dump site_version and quit.
 DL_file(file_url, site_version, DL, tmp)
