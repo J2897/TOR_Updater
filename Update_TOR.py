780f6642ef824d114052c788a29282669aa23251
John Shaw <j2897@mailinator.com>
2013-06-06 15:08:10 +0100

    Bug fixes and enhancements.
    Removed: Unnecessary comments in the 'find_site_ver()' procedure.
    Added: 'try' & 'except' and message boxes.
    Added: 'download_install()' procedure.

--- Update_TOR.py
+++ Update_TOR.py
@@ -1,34 +1,53 @@
 # Released under the GNU General Public License version 3 by J2897.
 
-import os
-
 def get_page(page):
 	import urllib2
 	source = urllib2.urlopen(page)
 	return source.read()
 
-def find_site_ver(page):
-	FT = page.find(target)		# First Target
-	if FT == -1:
-		return None
-	TB = page.find('tor-browser-', FT)	# Tor Browser
-	AB = page.find('>', TB)				# Angle Bracket
-	return page[TB:AB-1]				# tor-browser-2.3.25-6_en-US.exe
-
+title = 'TOR Updater'
 target = 'Tor Browser Bundle for Windows'
 url = 'https://www.torproject.org/projects/torbrowser.html.en'
 file_url = 'https://www.torproject.org/dist/torbrowser/'
-page = get_page(url)
-home = os.getenv('USERPROFILE')
-site_version = find_site_ver(page)
+
+try:
+	page = get_page(url)
+except:
+	page = None
+
+def msg_box(message, box_type):
+	import win32api
+	user_input = win32api.MessageBox(0, message, title, box_type)
+	return user_input
 
 def stop():
 	import sys
 	sys.exit()
 
-if site_version == None:
+if page == None:
+	msg_box('Could not download the page. You may not be connected to the internet.', 0)
 	stop()
 
+def find_site_ver(page):
+	T1 = page.find(target)
+	if T1 == -1:
+		return None
+	T2 = page.find('tor-browser-', T1)
+	T3 = page.find('>', T2)
+	return page[T2:T3-1]	# tor-browser-2.3.25-6_en-US.exe
+
+try:
+	site_version = find_site_ver(page)
+except:
+	msg_box('Could not search the page.', 0)
+	stop()
+
+if site_version == None:
+	msg_box('The search target has not been found on the page. The formatting, or the text on the page, may have been changed.', 0)
+	stop()
+
+import os
+home = os.getenv('USERPROFILE')
 tmp = os.getenv('TEMP') + '\\'
 DL = tmp + site_version
 PC = home + '\\TOR_Updater.dat'
@@ -72,13 +91,36 @@ def sub_proc(sz_exe, args):
 	stdout, stderr = p.communicate()
 	return p.returncode # is 0 if success
 
-# Check if Pickle Cache file exists...
+def download_install():
+	try:
+		DL_file(file_url, site_version, DL, tmp)
+	except:
+		msg_box('Could not download the file.', 0)
+		stop()
+	try:
+		sz_exe, args = get_sz(tmp, DL)
+	except:
+		msg_box('Could not construct the 7-Zip command.', 0)
+		stop()
+	try:
+		RC = sub_proc(sz_exe, args)
+		if RC == 0:
+			msg_box('Successfully updated to ' + site_version + '.', 0)
+		else:
+			msg_box('Successfully executed ' + site_version + '. But the installation may to have failed.', 0)
+	except:
+		msg_box('Failed to execute ' + site_version + '.', 0)
+		stop()
+	try:
+		dump(PC, site_version)
+	except:
+		msg_box('Could not dump site_version.', 0)
+		stop()
+
+# Check if the Pickle Cache file exists...
 if not os.path.isfile(PC):
 	# No:	Download and install TOR, create a Pickle Cache file in the home folder, dump site_version and quit.
-	DL_file(file_url, site_version, DL, tmp)
-	sz_exe, args = get_sz(tmp, DL)
-	sub_proc(sz_exe, args)
-	dump(PC, site_version)
+	download_install()
 	stop()
 
 # Load contents of Pickle Cache file (local_version).
@@ -99,14 +141,11 @@ def find_proc(exe):
 			return True
 
 while find_proc('vidalia.exe'):
-	user_input = win32api.MessageBox(0, 'There is a new version of the TOR Browser Bundle available. Please close TOR and press OK to continue.', 'TOR Updater', 1)
+	user_input = msg_box('There is a new version of the TOR Browser Bundle available. Please close TOR and press OK to continue.', 1)
 	if user_input == 1:
 		pass
 	elif user_input == 2:
 		stop()
 
 # Download and install TOR, dump site_version and quit.
-DL_file(file_url, site_version, DL, tmp)
-sz_exe, args = get_sz(tmp, DL)
-sub_proc(sz_exe, args)
-dump(PC, site_version)
+download_install()
