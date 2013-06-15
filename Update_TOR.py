cf824e133fff170d217cba9e797e8f29d472f4cd
John Shaw <j2897@mailinator.com>
2013-06-15 06:00:37 +0100

    Minor enhancements.
    Added: Visual output indicating the progress.
    Added: 5 second delay before ending the program.

--- Update_TOR.py
+++ Update_TOR.py
@@ -10,10 +10,16 @@ target = 'Tor Browser Bundle for Windows'
 url = 'https://www.torproject.org/projects/torbrowser.html.en'
 file_url = 'https://www.torproject.org/dist/torbrowser/'
 
+print 'Running:		' + title
+print 'Target:			' + target
+print 'URL:			' + url
+
 try:
 	page = get_page(url)
 except:
 	page = None
+else:
+	print 'Got page...'
 
 def msg_box(message, box_type):
 	import win32api
@@ -41,6 +47,8 @@ try:
 except:
 	msg_box('Could not search the page.', 0)
 	stop()
+else:
+	print 'Found:			' + site_version
 
 if site_version == None:
 	msg_box('The search target has not been found on the page. The formatting, or the text on the page, may have been changed.', 0)
@@ -87,6 +95,8 @@ def download_install():
 	except:
 		msg_box('Could not download the file.', 0)
 		stop()
+	else:
+		print 'Downloaded:		' + site_version
 
 	try:
 		sz_command = get_sz(tmp)
@@ -101,25 +111,46 @@ def download_install():
 	except:
 		msg_box('Failed to execute ' + site_version + '.', 0)
 		stop()
+	else:
+		print 'Update successful!'
 
 	try:
 		dump(PC, site_version)
 	except:
 		msg_box('Could not dump site_version.', 0)
 		stop()
+	else:
+		print 'Dumped cache:		' + PC
+
+def delay(sec):
+	import time
+	time.sleep(sec)
 
 # Check if the Pickle Cache file exists...
 if not os.path.isfile(PC):
 	# No:	Download and install TOR, create a Pickle Cache file in the home folder, dump site_version and quit.
+	print 'Cache file doesn\'t exist.'
+	print 'Installing TOR for the first time...'
 	download_install()
+	print 'Ending...'
+	delay(5)
 	stop()
 
 # Load contents of Pickle Cache file (local_version).
+print 'Loading cache...'
 local_version = load()
 
 # Check if the local_version numbers are in the site_version numbers...
-if clean(local_version).find(clean(site_version)) != -1:
+clean_local_version = clean(local_version)
+clean_site_version = clean(site_version)
+
+if clean_local_version.find(clean_site_version) != -1:
 	# Yes:	Quit.
+	print 'Local version:		' + clean_local_version
+	print 'Site version:		' + clean_site_version
+	print 'Match!'
+	print 'Ending...'
+	delay(5)
 	stop()
 
 # Check if TOR is running...
@@ -132,6 +163,7 @@ def find_proc(exe):
 			return True
 
 while find_proc('vidalia.exe'):
+	print 'TOR is running. Close TOR now!'
 	user_input = msg_box('There is a new version of the TOR Browser Bundle available. Please close TOR and press OK to continue.', 1)
 	if user_input == 1:
 		pass
@@ -140,3 +172,5 @@ while find_proc('vidalia.exe'):
 
 # Download and install TOR, dump site_version and quit.
 download_install()
+print 'Ending...'
+delay(5)
